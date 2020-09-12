f04f9e4771eeb457290a338d89659047446873b6
Lokendra Dangi <pachar.com@gmail.com>
2020-09-12 15:54:54 +0545

    firebase is setup

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDNNYfGvceYXxXmjNmhINg4-6nTnPPlWWg",
+  authDomain: "clone-6e7f5.firebaseapp.com",
+  databaseURL: "https://clone-6e7f5.firebaseio.com",
+  projectId: "clone-6e7f5",
+  storageBucket: "clone-6e7f5.appspot.com",
+  messagingSenderId: "421549295922",
+  appId: "1:421549295922:web:5d168220cbad0869d92f2b",
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
